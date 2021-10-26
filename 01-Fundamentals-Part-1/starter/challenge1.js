267e139610bdca668eb7af9e0a64fbd1105eb11f
Ely Mbaye <ely_mbaye@yahooe.com>
2021-10-26 10:31:55 -0700

    commit config

---
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,24 @@
+//Data one
+let marksHeigt = 1.69;
+let marksWeight = 78;
+
+let johnsHeigt = 1.95;
+let johnsWeight = 92;
+
+let marksBMI = marksWeight / marksHeigt ** 2;
+let johnBMI = johnsWeight / johnsHeigt ** 2;
+
+let markHigherBMI = marksBMI > johnBMI;
+console.log(markHigherBMI);
+// Data two
+marksHeigt = 1.88;
+marksWeight = 95;
+
+johnsHeigt = 1.76;
+johnsWeight = 85;
+
+marksBMI = marksWeight / marksHeigt ** 2;
+johnBMI = johnsWeight / johnsHeigt ** 2;
+
+markHigherBMI = marksBMI > johnBMI;
+console.log(markHigherBMI);
