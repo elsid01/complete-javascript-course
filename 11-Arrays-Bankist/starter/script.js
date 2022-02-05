73008d5c48dd5810b857e18603ffafb024edf839
Ely Mbaye <ely_mbaye@yahooe.com>
2022-02-04 16:21:23 -0800

    february 4 second commit

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -63,9 +63,12 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 
 
-const displayMovements = function(movements){
+const displayMovements = function(movements, sort = false){
   containerMovements.innerHTML ='';
-  movements.forEach(function(mov, i){
+
+  const movs = sort ? movements.slice().sort((a, b) => a -b) : movements;
+
+  movs.forEach(function(mov, i){
 
     const type = mov > 0 ?'deposit' :'withdrawal';
     const html = `
@@ -201,6 +204,16 @@ btnClose.addEventListener('click' , function(e){
      }
 
      inputCloseUsername.value = inputClosePin.value = '';
+})
+
+let sorted = false;
+btnSort.addEventListener('click', function(e){
+  e.preventDefault();
+
+  displayMovements(currentAccount.movements, !sorted);
+
+  sorted = !sorted;
+     
 })
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
